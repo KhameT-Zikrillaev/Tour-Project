d76a678233c410359bc9784c6f99a0b9ee0ebc18
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2024-05-26 18:31:34 +0300

    complete

---
+++ src/components/mainlayouts/section2.vue
@@ -0,0 +1,64 @@
+<script setup>
+import card2 from "@/components/cards/card2.vue";
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import datafiles2 from '@/views/DataFiles2.js'
+const breakpoints={
+      '400': {
+        slidesPerView: 1,
+        spaceBetween: 0,  
+      },  
+      '768': {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+
+      '1240': {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+      '1440': {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    }
+const modules = [Navigation]
+
+
+
+
+</script>
+
+
+<template>
+    <section class="section-2">
+      
+      <h3 class="section-2-title">{{ $t('Visa Support') }}</h3>
+         <p  class="section-2-info">{{ $t('Experience stress-free travel with our expert visa support. We provide seamless assistance and comprehensive guidance, ensuring a smooth journey') }}</p>
+
+  <swiper class="corusel-1" 
+:slides-per-view="1"
+:space-between="80"
+:breakpoints="breakpoints"
+:navigation="true" 
+:modules="modules"
+
+>
+
+<swiper-slide v-for="item in datafiles2" :key="item">
+  <card2 :info2="item"/>
+</swiper-slide>
+
+
+</swiper>
+
+    </section>
+</template>
+
+
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import './assets/styles/main.scss'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import {createI18n} from 'vue-i18n'
+
+import App from './App.vue'
+import router from './router'
+import en from '@/lenguage/en.json'
+import ru from '@/lenguage/ru.json'
+import uz from '@/lenguage/uz.json'
+const app = createApp(App)
+const pinia = createPinia()
+app.use(pinia)
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: {
+      en: en,
+      ru: ru,
+      uz: uz,
+    }
+  })
+
+app.use(i18n)
+app.mount('#app') 
+
+app.use(router)
+
